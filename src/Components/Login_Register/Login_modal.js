b541d9f960e04029f03c4dbea27403566134ffbd
jongseo05 <jourxy05@naver.com>
2025-01-10 02:16:02 +0900

    Content : 로그인 모달창 디자인
    모달창 구현 완료.
    로그인/회원가입 클릭 시 메인 페이지 위에 모달창 출력.

--- src/Components/Login_Register/Login_modal.js
+++ src/Components/Login_Register/Login_modal.js
@@ -1,44 +1,72 @@
 import React from "react";
 import "./Login_modal.css";
 import Logo from './img/Logo.png';
+import Kakao from './img/kakaotalki_icon.png';
+import Naver from './img/naver_icon.png';
+import Phone from './img/phone_icon.png';
+import { createPortal } from "react-dom";
 
-const Login_modal = () => {
-    return(
-        <div className = "Login_modal">
+const Login_modal = ({ onClose }) => {
 
-            <img src={Logo} alt = "번개장터 로고" className = "Login_logo"/>
-            <div className = "Login_title_section">
-               <p className = "Login_title">번개장터로 중고거래 시작하기</p>
-            </div>
-            <div className = "Login_explain_section">
-                <p className = "Login_explain"></p>
-            </div>
+    {/* 배경 클릭 시 모달 닫기 */}
+    const handleBackgroundClick = (e) => {
 
-            <div className = "Login_button_section">
+        if (e.target.className === "Login_back") {
+            onClose(false);
+        }
+    };
 
-                {/* 카카오 로그인 버튼 */}
-                <div className = "Login_button">
+    const modalRoot = document.getElementById("modal-root");
+
+    if (!modalRoot) {
+        console.error("#modal-root not found in DOM");
+        return null; // modal-root가 없으면 렌더링하지 않음
+    }
+
+    return createPortal(
+        <div className="Login_back" onClick={handleBackgroundClick}>
+            <div className="Login_modal" onClick={(e) => e.stopPropagation()}>
+                {/* 로고 */}
+                <img src={Logo} alt="번개장터 로고" className="Login_logo" />
+
+                {/* 제목 */}
+                <div className="Login_title_section">
+                    <p className="Login_title">번개장터로 중고거래 시작하기</p>
                 </div>
 
-                {/* 페이스북 로그인 버튼 */}
-                <div className = "Login_button">
+                {/* 설명 */}
+                <div className="Login_explain_section">
+                    <p className="Login_explain">간편하게 가입하고 상품을 확인하세요</p>
                 </div>
 
-                {/* 네이버 로그인 버튼 */}
-                <div className = "Login_button">
+                {/* 로그인 버튼들 */}
+                <div className="Login_button_section">
+                    {/* 카카오 로그인 버튼 */}
+                    <div className="Login_button">
+                        <img src={Kakao} className="Login_button_img" alt="카카오" />
+                        <p className="Login_button_text">카카오 로그인</p>
+                    </div>
+                    {/* 네이버 로그인 버튼 */}
+                    <div className="Login_button">
+                        <img src={Naver} className="Login_button_img" alt="네이버" />
+                        <p className="Login_button_text">네이버 로그인</p>
+                    </div>
+                    {/* 일반 로그인 버튼 */}
+                    <div className="Login_button">
+                        <img src={Phone} className="Login_button_img" alt="일반" />
+                        <p className="Login_button_text">일반 로그인</p>
+                    </div>
                 </div>
 
-                {/* 일반 로그인 버튼 */}
-                <div className = "Login_button">
+                <div className="Login_info_section">
+                    <p className="Login_info_text">
+                        도움이 필요하면 이메일 또는 고객센터1670-2910로 문의 부탁드립니다.
+                    </p>
                 </div>
-
             </div>
-
-            <div className = "Login_info_section">
-            </div>
-
-        </div>
-    )
-}
+        </div>,
+        modalRoot // HTML의 modal-root 요소에 렌더링
+    );
+};
 
 export default Login_modal;
--- src/Components/Top_navbar/Top_navbar.js
+++ src/Components/Top_navbar/Top_navbar.js
@@ -2,8 +2,7 @@ import './Top_navbar.css';
 import React from 'react';
 import Logo from './logo.png';
 import Star from './star.png';
-import { Link } from 'react-router-dom';
-
+import Login_modal from '../Login_Register/Login_modal'; // Login_modal 컴포넌트 가져오기
 
 function Top_navbar() {
 
@@ -14,64 +13,59 @@ function Top_navbar() {
         console.log("openLoginModal");
     }
 
-    
+    const closeLoginModal = () => {
+        setOpenLoginModal(false);
+        console.log("closeLoginModal");
+    }
 
-
-    return(
-        <div className = "Top_navbar_section">
+    return (
+        <div className="Top_navbar_section">
 
             <div className="Top_navabar_main_container">
 
-
-                {/*앱 다운로드 , 즐겨찾기 버튼*/}
+                {/* 앱 다운로드, 즐겨찾기 버튼 */}
                 <div className="Top_navbar_content_container">
-
-
                     <div className="Content_section">
 
-                        {/*앱 다운로드*/}
+                        {/* 앱 다운로드 */}
                         <div className="Button_section">
                             <div className="Image_section">
-                                <img src={Logo} alt="logo" className = "Image"/>
+                                <img src={Logo} alt="logo" className="Image" />
                             </div>
                             <div className="Text_section">
                                 <p>앱 다운로드</p>
                             </div>
                         </div>
 
-                        {/*즐겨찾기*/}
+                        {/* 즐겨찾기 */}
                         <div className="Button_section">
                             <div className="Image_section">
-                                <img src={Star} alt="logo" className = "Image"/>
+                                <img src={Star} alt="logo" className="Image" />
                             </div>
                             <div className="Text_section">
                                 <p>즐겨찾기</p>
                             </div>
                         </div>
-
-
                     </div>
                 </div>
 
-
-                {/*로그인,회원가입*/}
+                {/* 로그인, 회원가입 */}
                 <div className="Top_navbar_content_container">
-                   <div className="Login_Text_section">
-                          <p className = "Text_section" >로그인/회원가입</p>
-                   </div>
-
-                   <div className = "Login_Text_section">
-                            <p className = "Text_section">내 상점</p>
-                   </div>
-
-
+                    <button className="Login_Text_button" onClick={openLoginModal}>
+                        로그인/회원가입
+                    </button>
+                    <div className="Login_Text_section">
+                        <p className="Text_section">내 상점</p>
+                    </div>
                 </div>
-
-
             </div>
 
+            {/* Login_modal 표시 */}
+            {stateLoginModal && (
+                <Login_modal onClose={closeLoginModal} />
+            )}
         </div>
-    )
+    );
 }
 
 export default Top_navbar;
