f52fb3cf7582c6388b368a30c1d20b7c8c81ee84
jongseo05 <jourxy05@naver.com>
2025-01-27 02:59:26 +0900

    feat : 번개톡 페이지
    채팅방 디자인 , 채팅 아이콘 디자인 완료

---
+++ src/Pages/Lightning_talk/Chating_icon/UserIcon/UserIcon.js
@@ -0,0 +1,17 @@
+import './UserIcon.css';
+
+function User_chatIcon() {
+    return(
+        <div className="User_chatIcon_section">
+            <div className='User_chatIcon_time_container'>
+                오후 1:43
+            </div>
+            <div className="User_chatIcon_container">
+                ㅎㅇ
+            </div>
+
+        </div>
+    )
+}
+
+export default User_chatIcon;
